8d826335bde11862ea031b566cc356d54ae32db3
sunasunasun <xiaoyu.sun@ucalgary.ca>
2019-06-15 00:07:52 +0000

    add ReviewsControllerv

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show] do
+       resources :reviews, only: [:create]
+  end
+
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
@@ -28,9 +31,6 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
-  resources :products do
-    resources :reviews
-  end
 
 
 
