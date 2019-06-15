4e438853bc3316975959cf15ca66f44052007ecd
sunasunasun <xiaoyu.sun@ucalgary.ca>
2019-06-15 00:49:28 +0000

    add reviews and show all reviews for one product

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,21 @@
 class ReviewsController < ApplicationController
   def create
-    raise "Yay, I'm here!"
+     @product = Product.find(params[:product_id])
+    # c = @product.reviews.create!(allowed_params)
+
+    @review=@product.reviews.new(review_params)
+    # @review.product_id=@product.id
+    @review.user=current_user
+
+       redirect_to product_path(@product.id)
+
+
+  end
+
+  def review_params
+    params.require(:review).permit(
+      :rating,
+      :description
+    )
   end
 end
