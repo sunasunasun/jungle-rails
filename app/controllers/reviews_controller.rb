cd9bddc505821295c2728c2946fcfb7c7805882b
sunasunasun <xiaoyu.sun@ucalgary.ca>
2019-06-17 06:14:09 +0000

    Only allow logged in users to create reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,24 @@
 class ReviewsController < ApplicationController
+ before_action :require_login
   def create
-     @product = Product.find(params[:product_id])
-    # c = @product.reviews.create!(allowed_params)
+    @product = Product.find_by(id: params[:product_id])
+    @review = @product.reviews.new(review_params)
+    @review.user = current_user
 
-    @review=@product.reviews.new(review_params)
-    # @review.product_id=@product.id
-    @review.user=current_user
+    if @review.save!
+      redirect_to :back
+    else
+      @status = true
+      redirect_to :back
+    end
+  end
 
-       redirect_to product_path(@product.id)
-
-
+  def destroy
+    @review = Review.find_by(id: params[:id])
+    if @review.user_id == current_user.id
+      @review.destroy
+    end
+    redirect_to :back
   end
 
   def review_params
@@ -17,5 +26,14 @@ class ReviewsController < ApplicationController
       :rating,
       :description
     )
+  end
+
+    private
+
+  def require_login
+    unless current_user
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to '/login'
+    end
   end
 end
